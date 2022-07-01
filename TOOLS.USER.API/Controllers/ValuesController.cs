793d56ae45ad2aecc718ad2deb1d0be3cac8f193
Leonardo Almeida <Leo.Ferreira30@outlook.com>
2022-06-30 23:35:57 -0300

    Teste de cors

---
+++ TOOLS.USER.API/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TOOLS.USER.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [EnableCors("CorsPolicy")]
+        [HttpGet("/teste/security/activate/{codigo}/{usuarioId}")]
+        public async Task<IActionResult> teste(string codigo, int usuarioId)
+        {
+            return Ok("Funcionou");
+        }
+    }
+}
