953d617c2a6ddb1b0f3752371a42ddd644be2962
Leonardo Ferreira de Almeida <Leo.Ferreira30@outlook.com>
2022-07-17 01:46:02 -0300

    Update BaseController.cs
    Removido authorize do options
    
    Signed-off-by: Leonardo Ferreira de Almeida <Leo.Ferreira30@outlook.com>

--- TOOLS.USER.API/CONTROLLER/BASE/BaseController.cs
+++ TOOLS.USER.API/CONTROLLER/BASE/BaseController.cs
@@ -6,7 +6,7 @@ namespace TOOLS.USER.API.CONTROLLER.BASE;
 [Route("api/[controller]")] [ApiController]
 public class BaseController : ControllerBase
 {
-    [Authorize(Policy = "User")] [HttpOptions("/options")]
+    [HttpOptions("/options")]
     public async Task<IActionResult> Options()
     {
         return await Task.FromResult(Ok("Retorno das options da aplicação!"));
