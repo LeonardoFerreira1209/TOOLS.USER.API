aedfbeaba49ed4f62ac93785a98215a23d35f919
Leonardo Almeida <Leo.Ferreira30@outlook.com>
2022-07-31 10:16:03 -0300

    Ajuste cors

--- APPLICATION/APPLICATION/CONFIGURATIONS/ExtensionsConfigurations.cs
+++ APPLICATION/APPLICATION/CONFIGURATIONS/ExtensionsConfigurations.cs
@@ -447,7 +447,7 @@ public static class ExtensionsConfigurations
         {
             options.AddPolicy("CorsPolicy", policy =>
             {
-                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5173", "https://toolswebapp.netlify.app/").AllowCredentials();
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://toolswebapp.netlify.app/").AllowCredentials();
             });
         });
     }
